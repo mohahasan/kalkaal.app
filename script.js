d59217117a440ef87b1eacf1faf13c964a30b9ac
mohahasan <45252502+mohahasan@users.noreply.github.com>
2025-04-11 19:22:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    document.querySelector("nav ul").classList.toggle("active");
+  }
+  
+  function closeMenu() {
+    document.querySelector("nav ul").classList.remove("active");
+  }
+  
+  function downloadApp() {
+    window.location.href = "https://play.google.com/store/apps/details?id=com.kalkaal.exchange"; // 
+  }
+  
